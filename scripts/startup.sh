49f358c432438d0c627f829a60ddbdec4fb3c99d
HeHeSkyWolf <67125129+HeHeSkyWolf@users.noreply.github.com>
2026-07-30 22:51:29 +0800

    chore: move startup.sh into scripts/ directory

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,7 +4,7 @@ echo "Build the Workplace Network"
 docker network inspect workplace-net >/dev/null 2>&1 || docker network create workplace-net
 
 echo "Starting Workplace Nginx"
-cd nginx && docker-compose up -d && cd ..
+cd ../nginx && docker-compose up -d && cd ..
 
 echo "Starting Gogs"
-cd gogs && docker-compose up -d && cd ..
+cd ../gogs && docker-compose up -d && cd ..
