d14ddf10c222ff8f26be8264d8769494aee62423
HeHeSkyWolf <67125129+HeHeSkyWolf@users.noreply.github.com>
2026-07-31 22:52:50 +0800

    feat(scripts): add shutdown script for workspace

---
+++ scripts/shutdown.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Stopping Gogs"
+cd ../gogs && docker-compose down && cd ..
+
+echo "Stopping App-Proxy Nginx"
+cd ../app-proxy && docker-compose down && cd ..
+
+echo "Stopping Gateway Nginx"
+cd ../gateway && docker-compose down && cd ..
+
+echo "All containers stopped."
