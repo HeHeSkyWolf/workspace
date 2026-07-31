adc852d1ada2cea8bd92c2c1e1af16ec72a4664a
HeHeSkyWolf <67125129+HeHeSkyWolf@users.noreply.github.com>
2026-07-31 22:52:26 +0800

    fix(scripts): start gateway and app-proxy in startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,8 +3,11 @@
 echo "Build the Workplace Network"
 docker network inspect workplace-net >/dev/null 2>&1 || docker network create workplace-net
 
-echo "Starting Workplace Nginx"
-cd ../nginx && docker-compose up -d && cd ..
+echo "Starting Gateway Nginx"
+cd ../gateway && docker-compose up -d && cd ..
+
+echo "Starting App-Proxy Nginx"
+cd ../app-proxy && docker-compose up -d && cd ..
 
 echo "Starting Gogs"
 cd ../gogs && docker-compose up -d && cd ..
